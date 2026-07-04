c8290f9e216f1745eda9e0e5143c8b8248043960
samiii90 <muhammad15sami2005@gmail.com>
2026-07-04 22:36:28 +0500

    Update RootNavigator.tsx

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,18 +1,43 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import { RootStackParamList } from './types';
+
 import { HomeScreen } from '../screens/Home';
 import { SettingsScreen } from '../screens/Settings';
 import { AboutScreen } from '../screens/About';
+
+import { ViewerScreen } from '../screens/Viewer/ViewerScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const RootNavigator = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Settings" component={SettingsScreen} />
-      <Stack.Screen name="About" component={AboutScreen} />
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        animation: 'fade',
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+      />
+
+      <Stack.Screen
+        name="Viewer"
+        component={ViewerScreen}
+      />
+
+      <Stack.Screen
+        name="Settings"
+        component={SettingsScreen}
+      />
+
+      <Stack.Screen
+        name="About"
+        component={AboutScreen}
+      />
     </Stack.Navigator>
   );
 };
